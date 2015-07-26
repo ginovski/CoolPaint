b75431ea5007520ccca4f205a22d79588fdb9d59
radenkovn <radenkovn@yahoo.com>
2015-07-26 17:11:27 +0300

    Wrapped the circle tool

--- Source/js/tools/circleTool.js
+++ Source/js/tools/circleTool.js
@@ -1,9 +1,13 @@
-var circleTool = makeTool('Circle', null, null, circleToolMouseDown, null);
-var defaultradius=40;
+(function(){
+    var circleTool = makeTool('Circle', null, null, circleToolMouseDown, null),
+        defaultRadius=40;
 
-function circleToolMouseDown(){
-    ctx.beginPath();
-    ctx.arc(mousePositionX, mousePositionY, defaultradius, 0, 2 * Math.PI, false);
-    ctx.fill();
-    ctx.stroke();
-}
+    function circleToolMouseDown(){
+        ctx.beginPath();
+        ctx.arc(mousePositionX, mousePositionY, defaultRadius, 0, 2 * Math.PI, false);
+        ctx.fill();
+        ctx.stroke();
+    }
+    return circleTool;
+}());
+
