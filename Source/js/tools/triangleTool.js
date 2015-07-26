85b5a4798cb9966c6a059be1dd7bb9a8d9fb9a30
ginovski <sashko.ginovski@gmail.com>
2015-07-26 17:03:04 +0300

    Fix merge bugs

--- Source/js/tools/triangleTool.js
+++ Source/js/tools/triangleTool.js
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
 var triangleTool = makeTool('Triangle', triangleToolMouseDown, null, null, null);
-=======
-var triangleTool = makeTool('Triangle', null, null, triangleToolMouseDown, null);
 
->>>>>>> 72a6c82413d9b5fbd16e6c460e1ab2caef5a75d3
+var tempX = mousePositionX;
+var tempY = mousePositionY;
 
 function triangleToolMouseDown() {
-<<<<<<< HEAD
     ctx.beginPath();
     ctx.moveTo(mousePositionX, mousePositionY);
     ctx.lineTo(tempX + 100, tempY);
     ctx.lineTo(tempX , tempY + 100);
     ctx.closePath();
-=======
-    var tempX = mousePositionX;
-    var tempY = mousePositionY;
-    ctx.beginPath();
-    ctx.moveTo(tempX, tempY);
-    ctx.lineTo(tempX + 100, tempY);
-    ctx.lineTo(tempX , tempY + 100);
-    ctx.closePath();
-    ctx.fill();
-    ctx.stroke();
->>>>>>> 72a6c82413d9b5fbd16e6c460e1ab2caef5a75d3
 }
