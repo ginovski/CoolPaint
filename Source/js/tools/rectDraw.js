6a7140c6f9ff08713acb038fd6056d87dbf57509
ginovski <sashko.ginovski@gmail.com>
2015-07-26 16:46:18 +0300

    Fix menu bug and drawRect added

--- Source/js/tools/rectDraw.js
+++ Source/js/tools/rectDraw.js
@@ -1,9 +1,10 @@
-var rectTool = makeTool('Rectangle', null, null, brushToolMouseDown, null);
+var rectTool = makeTool('Rectangle', null, null, rectToolMouseDown, null);
 var width = 50;
 var height = 50;
 
 function rectToolMouseDown(){
-    ctx.rect(mousePositionX - width/2 ,mousePositionY- width/2,width,height);
+    ctx.beginPath();
+    ctx.rect(mousePositionX ,mousePositionY,width,height);
     ctx.stroke();
     ctx.fill();
 }
