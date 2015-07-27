bb43678ad2270fee524d1e51c6342c78e84589fa
Ivaylo Andonov <ivailo.kolev.andonov@gmail.com>
2015-07-27 13:01:00 +0300

    Added workin palette( opacity slider and paint api left)
    The last commit is saved if you want to revert

---
+++ Source/js/tools/pickColor.js
@@ -0,0 +1,24 @@
+﻿var swatch = document.getElementById('swatch');
+swatch.addEventListener('click', setSwatch);
+
+function setSwatch(e) {
+    var swatch = e.target;
+
+    setColor(swatch.style.backgroundColor);
+
+}
+
+function setColor(color) {
+    ctx.fillStyle = color;
+    ctx.strokeStyle = color;
+
+}
+
+var colors = ['black', 'grey', 'white', 'red', 'orange', 'yellow', 'yellowgreen', 'cyan', 'green', 'blue', 'violet', 'indigo'];
+for (var i = 0, n = colors.length ;i < n; i+=1) {
+    var swatch = document.createElement('div');
+    swatch.className = 'color';
+    swatch.style.backgroundColor = colors[i];
+    swatch.addEventListener('click', setSwatch);
+    document.getElementById('colors').appendChild(swatch);
+}
--- Source/js/ui-components/colorsSlider.js
+++ Source/js/ui-components/colorsSlider.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $('#arrow-top').mousedown(function () {
         $('#top-options').slideToggle(300);
-        $('#colorPicker').ColorPicker();
     });
 });
