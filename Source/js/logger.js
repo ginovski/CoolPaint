4f42b23248fa8ed7a02af420b6a557d895e11cca
Yulia Tenincheva <yulia.ten@dezigner.ru>
2015-07-30 12:53:39 +0300

    Some bug fixes on the logger

--- Source/js/logger.js
+++ Source/js/logger.js
@@ -7,7 +7,7 @@ var redoActions = [];
 var logAction = function (tmpImage) {
     if(actions.indexOf(tmpImage)){
         actions.push(tmpImage);
-        redoActions.push(tmpImage);
+        redoActions = [];
     }
 
     // TODO: Optimization - Save only the last 10 actions?
@@ -15,31 +15,34 @@ var logAction = function (tmpImage) {
 };
 
 function undoAction() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    redoActions.push(actions.pop());
-
     if(actions.length > 0){
-        var lastSaved = actions.pop(); // pop this to get the data and push it back again later - FIXME if better solution comes on
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        redoActions.push(actions.pop());
 
-        var img = new Image();
-        img.src = lastSaved;
-        actions.push(lastSaved);
+        if(actions.length > 1){
 
-        ctx.drawImage(img, 0, 0);
+            var lastSaved = actions.pop(); // pop this to get the data and push it back again later - FIXME if better solution comes on
+
+            var img = new Image();
+            img.src = lastSaved;
+            actions.push(lastSaved);
+
+            ctx.drawImage(img, 0, 0);
+        }
     }
 }
 
 function redoAction(){
-    var lastUndo = redoActions.pop();
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    var img = new Image();
-    img.src = lastUndo;
-
-    ctx.drawImage(img, 0, 0);
-    redoActions.push(lastUndo);
-
     if(redoActions.length > 0){
+        var lastUndo = redoActions.pop();
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        var img = new Image();
+        img.src = lastUndo;
+
+        ctx.drawImage(img, 0, 0);
+        redoActions.push(lastUndo);
+
         actions.push(redoActions.pop());
     }
 }
